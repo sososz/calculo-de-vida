68129e15e0d8ac6d7042244677c6b28449034395
Maria Eduarda Soares <mariaeduardasol2017@gmail.com>
2025-06-12 02:47:55 -0300

    teste

--- api_simu/teste.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-resposta = requests.get("https://api.teleport.org/api/urban_areas/slug:paris/details/")
-
-if resposta.status_code == 200:
-    dados = resposta.json()
-    print(dados)  # Mostra o JSON que a API retornou
-else:
-    print("Erro ao acessar a API.")
